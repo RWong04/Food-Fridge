dbe50137fd798cef8f56c09eb73567485f16c514
RWong04 <wongchonghin79@gmail.com>
2025-04-27 14:59:21 +0800

    Fix bug to stop.sh

--- stop.sh
+++ stop.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -e
 
+COMPOSE_FILE="docker-compose.yml"  # 你的 compose 檔案路徑
+PROJECT_NAME="final_project"       # 專案名稱(需與 compose 的 container 前綴一致)
+
 echo "=== 停止所有運行中之容器 ==="
 
-docker-compose -f "docker/docker-compose.issuer.yml" -p issuer down --remove-orphans
+docker-compose -f ${COMPOSE_FILE} -p ${PROJECT_NAME} down --remove-orphans
 
 echo "=== 所有容器已停止 ==="
