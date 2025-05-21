1d6b1bfdc8590ddb151734974ee072dfc2305af6
angel <chloe.tseng.tw@gmail.com>
2025-05-21 17:11:58 +0800

    add

--- web/Food_Waste_Fridge/settings.py
+++ web/Food_Waste_Fridge/settings.py
@@ -132,8 +132,10 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-COMPRESS_ROOT = BASE_DIR / 'static'
 
-COMPRESS_ENABLED = True
 
-STATICFILES_FINDERS = ('compressor.finders.CompressorFinder',)
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder',
+)
