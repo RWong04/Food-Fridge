56f7448971e6cdf8c20f2974877cd754c8f6a555
Today-Asked <mao930710@gmail.com>
2025-05-21 13:51:12 +0800

    merged navbar and footer into base.html; remove password authenticator

--- web/Food_Waste_Fridge/settings.py
+++ web/Food_Waste_Fridge/settings.py
@@ -95,25 +95,6 @@ DATABASES = {
 }
 AUTH_USER_MODEL = 'food_fridge.CustomUser'
 
-# Password validation
-# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
-
-
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:16701",
     "http://127.0.0.1:16701",
